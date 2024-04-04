030b237cabaad8495f19cfeb46bbf424fca43bca
Gabriel Estéfono <gabrielestefono@hotmail.com>
2024-04-04 14:57:29 -0300

    Criando segunda migração

--- projeto_01/devtraining/src/database/orm-cli-config.ts
+++ projeto_01/devtraining/src/database/orm-cli-config.ts
@@ -1,12 +1,14 @@
 import { CreateCoursesTable1712244542866 } from 'src/migrations/1712244542866-CreateCoursesTable';
 import { dataSourceOptions } from './database.module';
 import { DataSource } from "typeorm";
+import { CreateTagsTable1712246021569 } from 'src/migrations/1712246021569-CreateTagsTable';
 
 export const dataSource = new DataSource({
 	...dataSourceOptions,
 	synchronize: false,
 	migrations: [
-		CreateCoursesTable1712244542866
+		CreateCoursesTable1712244542866,
+		CreateTagsTable1712246021569
 	]
 })
 
