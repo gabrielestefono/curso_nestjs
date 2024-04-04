54dba26de85402b550081183982995a5d5690711
Gabriel Estéfono <gabrielestefono@hotmail.com>
2024-04-04 15:19:55 -0300

    Criando migração do relacionamento

--- projeto_01/devtraining/src/database/orm-cli-config.ts
+++ projeto_01/devtraining/src/database/orm-cli-config.ts
@@ -2,13 +2,15 @@ import { CreateCoursesTable1712244542866 } from 'src/migrations/1712244542866-Cr
 import { dataSourceOptions } from './database.module';
 import { DataSource } from "typeorm";
 import { CreateTagsTable1712246021569 } from 'src/migrations/1712246021569-CreateTagsTable';
+import { CoursesTagsTable1712254189974 } from 'src/migrations/1712254189974-CoursesTagsTable';
 
 export const dataSource = new DataSource({
 	...dataSourceOptions,
 	synchronize: false,
 	migrations: [
 		CreateCoursesTable1712244542866,
-		CreateTagsTable1712246021569
+		CreateTagsTable1712246021569,
+		CoursesTagsTable1712254189974
 	]
 })
 
