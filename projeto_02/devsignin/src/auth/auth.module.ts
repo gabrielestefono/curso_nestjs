a04e54195a6f2def8583378af515b753cbb6d633
Gabriel Estéfono <gabrielestefono@hotmail.com>
2024-04-15 11:18:59 -0300

    Finalizando parte 03 Autenticação

--- projeto_02/devsignin/src/auth/auth.module.ts
+++ projeto_02/devsignin/src/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UsersSchema } from 'src/users/schema/users.schema';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   imports: [MongooseModule.forFeature([
@@ -20,7 +21,7 @@ import { JwtModule } from '@nestjs/jwt';
     }
   })
 ],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   exports: [AuthService]
 })
 export class AuthModule {}
